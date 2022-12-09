4142b2690e0d2dec13a64dc60155af1196eb0dd4
Alex Diaz <alex@akdev.xyz>
2022-12-08 22:15:56 -0500

    fix: try to use build script
    debugging github actions takes a long time :-(

---
+++ util/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+main() {
+    image_name="$1"
+    image_builddir="$PWD/images/${image_name}"
+
+    if [ ! -d "images/${image_name}" ]; then
+        printf 'could not find image "%s"\n"' "${image_name}"
+        return 1
+    fi
+
+
+    image_id="$(podman build "${image_builddir}" | tail -1)"
+    image_tag="${image_name}:latest"
+
+    podman tag "${image_id}" "localhost/${image_tag}"
+
+    printf 'created: %s\n' "${image_tag}"
+}
+
+main "$@"
