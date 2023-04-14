8ba2b1c3be756c6e056eb448e224707023dc5457
Alex Diaz <alex@akdev.xyz>
2023-04-14 10:46:41 -0400

    feat: try to base build off ublue

--- util/publish.sh
+++ util/publish.sh
@@ -3,13 +3,14 @@ set -x
 
 main() {
     image_name="$(./util/build.sh "${1}" | awk '{print $2}' | awk '{print $1}' FS=':')"
+    IMAGE_TAGS=(latest "$(date +%Y%m%d)" "$(git rev-parse HEAD)")
+    IMAGE_REGISTRY="${IMAGE_REGISTRY:-ghcr.io/akdev1l/ostree-images}"
 
     for tag in ${IMAGE_TAGS}; do
-        podman tag "${image_name}" "${IMAGE_REGISTRY}/${image_name}:${tag}"
-        podman push "${image_name}" "${IMAGE_REGISTRY}/${image_name}:${tag}"
+        podman tag "${image_name}:latest" "${IMAGE_REGISTRY}/${image_name}:${tag}"
+        podman push "${image_name}:latest" "${IMAGE_REGISTRY}/${image_name}:${tag}"
         echo "published: ${IMAGE_REGISTRY}/${image_name}:${tag}"
     done
-
 }
 
 main "$@"
